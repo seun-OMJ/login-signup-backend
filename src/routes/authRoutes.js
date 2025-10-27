f5832a1889073411ef7f35802554b12f022900cd
Seun OMJ <omojolaos@gmail.com>
2025-10-27 11:19:15 -0500

    worked on the login post

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,9 @@
-import express from 'express'
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs';
 import dotenv from 'dotenv';
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import { createUser, getUserByEmail } from '../DatabaseCode/databaseQueries.js';
 dotenv.config();
-import { createUser, getUserByEmail } from '../DatabaseCode/databaseQueries.js'
 const router = express.Router()
 
 router.post('/signup', async (req, res) => {
@@ -11,18 +11,43 @@ router.post('/signup', async (req, res) => {
     console.log(req.body)
     try {
         const existingUser = await getUserByEmail(email)
-        if(existingUser){
-            return res.status(400).send({message:"user already exists"})
+        if (existingUser) {
+            return res.status(400).send({ message: "user already exists" })
         }
         const hashedPassword = bcrypt.hashSync(password, 7)
-        const newUser =  await createUser(firstName, lastName, email, hashedPassword)
+        const newUser = await createUser(firstName, lastName, email, hashedPassword)
         const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, { expiresIn: '24h' })
         res.status(201).json({
             message: "User created successfully",
             user: newUser,
             token: token
-});
-        
+        });
+
+    } catch (error) {
+        console.log(error.message)
+        res.sendStatus(501)
+    }
+})
+
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body
+    console.log(req.body)
+    try {
+        const existingUser = await getUserByEmail(email)
+        if (!existingUser) {
+            return res.status(400).send({ message: "invalid email" })
+        }
+        const passwordIsValid = bcrypt.compareSync(password, existingUser.password)
+        if (!passwordIsValid) {
+            return res.status(401).send({ message: "Invalid login attempt!" })
+        }
+        const token = jwt.sign({ id: existingUser.id }, process.env.JWT_SECRET, { expiresIn: '24h' })
+        res.status(201).json({
+            message: "Login attempt successful",
+            token: token
+        });
+
     } catch (error) {
         console.log(error.message)
         res.sendStatus(501)
